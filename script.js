ffe391744fefb082f3285d2b51bcd6fcda2c28c4
unknown <bgsdewoo20@gmail.com>
2024-09-17 06:38:41 +0700

    Mengenal Tipe data string

---
+++ script.js
@@ -0,0 +1,10 @@
+//topik pengenalan data string//
+
+// const namaDepan ='Bagus'//penggunaan yg benar
+// const namaBelakang ="Sadewo"//ini juga benar
+// const ngaco = "Ini salah'//ini error!
+
+//beberapa kasus dalam penggunaan string
+// let caseSatu = 'saya mengucapkan "Selamat"'; //ini bisa
+// let caseDua = "I Can't Sleep"; //ini bisa juga
+// console.log(caseSatu, caseDua);
