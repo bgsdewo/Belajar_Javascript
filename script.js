fceaa453f907325bad3227a89f22387d2ec87add
unknown <bgsdewoo20@gmail.com>
2024-09-17 06:54:29 +0700

    Penjelasan Index Pada String Dan Panjang Karakter

--- script.js
+++ script.js
@@ -8,3 +8,15 @@
 // let caseSatu = 'saya mengucapkan "Selamat"'; //ini bisa
 // let caseDua = "I Can't Sleep"; //ini bisa juga
 // console.log(caseSatu, caseDua);
+
+//topik Penjelasan Index Pada String Dan Panjang Karakter
+
+// let nama = "Bagus";
+// //cara mencari tau indeks ke 0 dalam variabel nama itu apa?
+// console.log(nama[0]);
+// //cara mencari tau panjang atau ada berapa karakter dalam variabel nama
+// console.log(nama.length);
+// //cara asign baru dari variabel nama
+// nama = nama + " Sadewo";
+// //spasi dihitung karakter
+// console.log(nama.length);
