3fec0a61c3b04c6d7ee6feabaf14bf3a93413777
unknown <bgsdewoo20@gmail.com>
2024-09-17 10:51:44 +0700

    Mengenal Struktur Data Array

---
+++ script.js
@@ -0,0 +1,8 @@
+//topic Mengenal Struktur Data Array
+
+//Membuat Array
+let peserta = []; //array kosong
+
+let pesertaString = ["Bagus", "Sadewo"]; //array String
+let pesertaAngka = [1, 2, 3]; //array angka
+let campuran = ["Bagus", 1, null]; //array canpuran
