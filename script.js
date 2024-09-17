3970e0cea48c98f902069a4ba8e29be45ac28866
unknown <bgsdewoo20@gmail.com>
2024-09-17 09:26:46 +0700

    Belaajr pengkondisian Swicth

--- script.js
+++ script.js
@@ -71,3 +71,33 @@
 // } else {
 //   console.log("Masuk Ke Server");
 // }
+
+// topic Switch Sebagai Alternatif If Statement
+
+const day = prompt("Masukkan angka hari (1-7):");
+
+switch (day) {
+  case "1":
+    console.log("Hari ini adalah Senin");
+    break;
+  case "2":
+    console.log("Hari ini adalah Selasa");
+    break;
+  case "3":
+    console.log("Hari ini adalah Rabu");
+    break;
+  case "4":
+    console.log("Hari ini adalah Kamis");
+    break;
+  case "5":
+    console.log("Hari ini adalah Jumat");
+    break;
+  case "6":
+    console.log("Hari ini adalah Sabtu");
+    break;
+  case "7":
+    console.log("Hari ini adalah Minggu");
+    break;
+  default:
+    console.log("Angka yang dimasukkan tidak valid");
+}
