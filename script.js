ddccc61de5dada02f9e3097bbde546fbb2f1347b
unknown <bgsdewoo20@gmail.com>
2024-09-17 08:29:20 +0700

    Console Alert Dan Promp

--- script.js
+++ script.js
@@ -12,12 +12,21 @@
 //topicPerbedaan Jumlah Sama Dengan Dua Dan Tiga
 
 //contoh dalam double equals atau(==)
-console.log(55 == 55); //true
-console.log("b" == "c"); //false
-console.log("55" == 55); //true tapi ini salah
-console.log(0 == ""); //true
-console.log(true == false); //false
-console.log(0 == false); //false
-console.log(null == undefined); //true
+// console.log(55 == 55); //true
+// console.log("b" == "c"); //false
+// console.log("55" == 55); //true tapi ini salah
+// console.log(0 == ""); //true
+// console.log(true == false); //false
+// console.log(0 == false); //false
+// console.log(null == undefined); //true
 
 //solusi dari kejanggalan diatas gunakanlah === dan !==
+
+//beberapa jenis console,alert,dan prompt
+// console.log("contoh dari consolog");
+// console.warn("contoh dari consoleWarn");
+// console.error("contoh dari consoleError");
+// //contoh dari alert
+// alert("contoh dari alert");
+// //contoh dari prompt
+// console.log(prompt("ini contoh dari prompt"));
