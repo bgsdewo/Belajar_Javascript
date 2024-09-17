017d7885095040bb137f52a4a387f8aad0b25afc
unknown <bgsdewoo20@gmail.com>
2024-09-17 08:05:56 +0700

    Operator pembanding

---
+++ script.js
@@ -0,0 +1,10 @@
+//Javascript Logic dalam pembuat keputusan
+
+//topic operator pembanding
+
+//Lebih dari
+// console.log(12 > 11);
+// //Kurang dari
+// console.log(10 < 11);
+// //bernilai tidak sama dengan
+// console.log("dan masih banyak lainnya");
