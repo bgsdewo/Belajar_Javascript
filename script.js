6f51c385926ae91dd2c512ffe969be712282c23e
unknown <bgsdewoo20@gmail.com>
2024-09-17 11:07:40 +0700

    Mendapatkan Nilai Dengan Index Dan Mengubah Isinya

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 //topic Mengenal Struktur Data Array
 
 //Membuat Array
-let peserta = []; //array kosong
+// let peserta = []; //array kosong
 
-let pesertaString = ["Bagus", "Sadewo"]; //array String
-let pesertaAngka = [1, 2, 3]; //array angka
-let campuran = ["Bagus", 1, null]; //array canpuran
+// let pesertaString = ["Bagus", "Sadewo"]; //array String
+// let pesertaAngka = [1, 2, 3]; //array angka
+// let campuran = ["Bagus", 1, null]; //array canpuran
+
+// topic Mendapatkan Nilai Dengan Index Dan Mengubah Isinya
+let warna = ["hijau", "kuning", "kelab", "bu"];
+console.log(warna);
+//me re asign indeks 2 dalam array
+warna[2] = "kelabu";
+console.log(warna);
+//merubah susunan indeks
+let warnaa = ["hijau", "merah", "biru", "kuning"];
+warnaa[0] = "kuning";
+console.log(warnaa);
