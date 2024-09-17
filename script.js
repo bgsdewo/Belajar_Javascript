44ac91a5a1b77902590da858ab0228be73f38f28
unknown <bgsdewoo20@gmail.com>
2024-09-17 08:17:04 +0700

    Perbedaan Jumlah Sama Dengan Dua Dan Tiga

--- script.js
+++ script.js
@@ -8,3 +8,16 @@
 // console.log(10 < 11);
 // //bernilai tidak sama dengan
 // console.log("dan masih banyak lainnya");
+
+//topicPerbedaan Jumlah Sama Dengan Dua Dan Tiga
+
+//contoh dalam double equals atau(==)
+console.log(55 == 55); //true
+console.log("b" == "c"); //false
+console.log("55" == 55); //true tapi ini salah
+console.log(0 == ""); //true
+console.log(true == false); //false
+console.log(0 == false); //false
+console.log(null == undefined); //true
+
+//solusi dari kejanggalan diatas gunakanlah === dan !==
