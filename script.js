5da42b97d3ece4dc9de30c7e91a291470fc275e4
unknown <bgsdewoo20@gmail.com>
2024-09-17 11:15:08 +0700

    Method Array Push Dan Pop

--- script.js
+++ script.js
@@ -8,12 +8,27 @@
 // let campuran = ["Bagus", 1, null]; //array canpuran
 
 // topic Mendapatkan Nilai Dengan Index Dan Mengubah Isinya
-let warna = ["hijau", "kuning", "kelab", "bu"];
-console.log(warna);
-//me re asign indeks 2 dalam array
-warna[2] = "kelabu";
-console.log(warna);
-//merubah susunan indeks
-let warnaa = ["hijau", "merah", "biru", "kuning"];
-warnaa[0] = "kuning";
-console.log(warnaa);
+// let warna = ["hijau", "kuning", "kelab", "bu"];
+// console.log(warna);
+// //me re asign indeks 2 dalam array
+// warna[2] = "kelabu";
+// console.log(warna);
+// //merubah susunan indeks
+// let warnaa = ["hijau", "merah", "biru", "kuning"];
+// warnaa[0] = "kuning";
+// warnaa[3] = "hijau";
+// console.log(warnaa);
+
+//topic Method Array Push Dan Pop
+
+// let barbel = [];
+// //menggunakan method push dalam array
+// barbel.push(20);
+// console.log(barbel);
+// barbel.push(40);
+// console.log(barbel);
+// barbel.push(60, 80, 100);
+// console.log(barbel);
+// //menggunakan method pop dalam array
+// barbel.pop();
+// console.log(barbel);
