a1d6a13a8c78c7f0707d4f0871509e60bad6a307
unknown <bgsdewoo20@gmail.com>
2024-09-18 15:37:15 +0700

    topic Perulangan Menggunakan Perintah While

--- script.js
+++ script.js
@@ -16,22 +16,31 @@
 // }
 
 //topic Mendapatkan Data Dari Nested Array Dengan Nested Loop
-const nestedArrayRow = [
-  ["Abidas", 75000000, "red", "blue"],
-  ["Niiko", 1200000, "black", "white"],
-  ["Jox", 3000000, "blue", "white"],
-];
+// const nestedArrayRow = [
+//   ["Abidas", 75000000, "red", "blue"],
+//   ["Niiko", 1200000, "black", "white"],
+//   ["Jox", 3000000, "blue", "white"],
+// ];
 
-//cara salah
+// //cara salah
+// // for (let i = 0; i < nestedArrayRow.length; i++) {
+// //   console.log(i);
+// // }
+// //cara benar
 // for (let i = 0; i < nestedArrayRow.length; i++) {
-//   console.log(i);
+//   const row = nestedArrayRow[i];
+//   //   console.log(row);
+//   console.log(`Baris array ke  ${i + 1}`);
+//   for (let j = 0; j < row.length; j++) {
+//     console.log(row[j]);
+//   }
 // }
-//cara benar
-for (let i = 0; i < nestedArrayRow.length; i++) {
-  const row = nestedArrayRow[i];
-  //   console.log(row);
-  console.log(`Baris array ke  ${i + 1}`);
-  for (let j = 0; j < row.length; j++) {
-    console.log(row[j]);
-  }
+
+//ganti topic Perulangan Menggunakan Perintah While
+const password = "Pass123";
+
+let gues = prompt("enter your password");
+while (gues !== password) {
+  gues = prompt("enter the password");
 }
+alert("Password Benar");
