0f2ea88bdc412cb8d47b95b9270342cd9f898076
unknown <bgsdewoo20@gmail.com>
2024-09-18 11:24:01 +0700

    Pengenalan Nested Loop

--- script.js
+++ script.js
@@ -7,10 +7,10 @@
 
 //topic Pengenalan Nested Loop
 
-const pilihan = ["A", "B", "C", "D"];
-for (let i = 1; i <= 10; i++) {
-  console.log(`${i}. Soal nomor ${i}`);
-  for (let j = 0; j < pilihan.length; j++) {
-    console.log(`  ${pilihan[j]}.  Pilihan Jawaban`);
-  }
-}
+// const pilihan = ["A", "B", "C", "D"];
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${i}. Soal nomor ${i}`);
+//   for (let j = 0; j < pilihan.length; j++) {
+//     console.log(`  ${pilihan[j]}.  Pilihan Jawaban`);
+//   }
+// }
