42f8e5c96a8825068717b1c4fc418374c77653a3
unknown <bgsdewoo20@gmail.com>
2024-09-18 10:00:19 +0700

    Cara Memanggil Data Dari Object

--- script.js
+++ script.js
@@ -22,3 +22,27 @@
 //   },
 // };
 // console.log(sepatuAbidas);
+
+//Topic Cara Memanggil Data Dari Object
+
+// const sepatuAbidas = {
+//   harga: 75000000,
+//   isReady: true,
+//   color: ["red", "blue", "yellow", "green"],
+//   locationStock: {
+//     indonesia: 43,
+//     malaysia: 53,
+//     singapore: 63,
+//   },
+// };
+
+// //cara tidak disarankan
+// console.log(sepatuAbidas["harga"]);
+// //cara disarankan
+// console.log(sepatuAbidas.harga);
+// //ada beberapa case yang bisa mengambil isi object sprti ini
+// console.log(sepatuAbidas["is" + "Ready"]);
+// //cara mengakses jika ada object didalam object
+// console.log(sepatuAbidas.locationStock.indonesia);
+// //cara mengakses array didalam object
+// console.log(sepatuAbidas.color[0]);
