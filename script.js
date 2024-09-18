85a9113ce19658d4b95a53d28e70f2f4ba41594c
unknown <bgsdewoo20@gmail.com>
2024-09-18 11:23:47 +0700

    Pengenalan Nested Loop

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 //topic Mendapatkan Data Dari Array Dengan Perulangan
 
-const animals = ["cat", "dog", "bird"];
-for (let i = 0; i < animals.length; i++) {
-  console.log(i, animals[i]);
+// const animals = ["cat", "dog", "bird"];
+// for (let i = 0; i < animals.length; i++) {
+//   console.log(i, animals[i]);
+// }
+
+//topic Pengenalan Nested Loop
+
+const pilihan = ["A", "B", "C", "D"];
+for (let i = 1; i <= 10; i++) {
+  console.log(`${i}. Soal nomor ${i}`);
+  for (let j = 0; j < pilihan.length; j++) {
+    console.log(`  ${pilihan[j]}.  Pilihan Jawaban`);
+  }
 }
