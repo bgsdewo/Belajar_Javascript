91ad98fe3766d8c1ecdaf589ce0b52d9d338e384
unknown <bgsdewoo20@gmail.com>
2024-09-18 09:51:19 +0700

    Cara Membuat Struktur Data Object

--- script.js
+++ script.js
@@ -2,3 +2,23 @@
 
 //topic Apa itu Object
 //cuman lampiran gambar saja
+
+//topic Cara Membuat Struktur Data Object
+// const dataSaya = {
+//   namaDepan: "Bagus",
+//   namaBelakang: "Sadewo",
+//   anakKe: "Dua",
+// };
+// console.log(dataSaya);
+// //contoh lainnya dari object
+// const sepatuAbidas = {
+//   harga: 75000000,
+//   isReady: true,
+//   color: ["red", "blue", "yellow", "green"],
+//   locationStock: {
+//     indonesia: 43,
+//     malaysia: 53,
+//     singapore: 63,
+//   },
+// };
+// console.log(sepatuAbidas);
