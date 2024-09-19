e7253e69d9e634e57dd50a3dd27f8648b7af3995
unknown <bgsdewoo20@gmail.com>
2024-09-19 10:02:17 +0700

    function Sebagai Argument Function Lain

--- script.js
+++ script.js
@@ -70,3 +70,15 @@
 // };
 
 // console.log(hasilPerkalian(10));
+
+//function Sebagai Argument Function Lain
+// function duaKali(func) {
+//   func();
+//   func();
+// }
+
+// function lemparDadu() {
+//   const hasil = Math.floor(Math.random() * 6) + 1;
+//   console.log(hasil);
+// }
+// duaKali(lemparDadu);
