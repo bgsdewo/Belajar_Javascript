a0bd431a64a9a0db4dd6f108f06a2577f6010723
unknown <bgsdewoo20@gmail.com>
2024-09-19 18:48:51 +0700

    Foreach Method

---
+++ script.js
@@ -0,0 +1,33 @@
+//topic callback function dan method milik array
+
+//topic For Each Method
+
+//contoh sederhana dari for Each
+// const angka = [1, 2, 3, 4, 5, 6];
+// function print(element) {
+//   console.log(element);
+// }
+// angka.forEach(print);
+
+//cara ke dua
+// angka.forEach(function (el) {
+//   if (el % 2 === 0) {
+//     console.log(el);
+//   }
+// });
+
+//cara ketiga
+// const animes = [
+//   { title: "Attack on Titan", rating: 9.1 },
+//   { title: "Fullmetal Alchemist: Brotherhood", rating: 9.2 },
+//   { title: "Naruto", rating: 8.3 },
+//   { title: "One Piece", rating: 8.8 },
+//   { title: "Death Note", rating: 8.9 },
+//   { title: "Demon Slayer", rating: 8.7 },
+//   { title: "My Hero Academia", rating: 8.4 },
+//   { title: "Sword Art Online", rating: 7.6 },
+// ];
+
+// animes.forEach(function (anime) {
+//   console.log(`${anime.title} - ${anime.rating}/100`);
+// });
