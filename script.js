820c5a5d40f3825d80c15cc48c4ff24d78636e70
unknown <bgsdewoo20@gmail.com>
2024-09-19 19:04:01 +0700

    Map Method

--- script.js
+++ script.js
@@ -31,3 +31,28 @@
 // animes.forEach(function (anime) {
 //   console.log(`${anime.title} - ${anime.rating}/100`);
 // });
+
+//topic Maps method
+// const angka = [1, 2, 3, 4, 5, 6];
+// const angkaDouble = angka.map(function (num) {
+//   return num * 2;
+// });
+// console.log(angkaDouble);
+
+// //contoh lain
+
+// const animes = [
+//   { title: "Attack on Titan", rating: 9.1 },
+//   { title: "Fullmetal Alchemist: Brotherhood", rating: 9.2 },
+//   { title: "Naruto", rating: 8.3 },
+//   { title: "One Piece", rating: 8.8 },
+//   { title: "Death Note", rating: 8.9 },
+//   { title: "Demon Slayer", rating: 8.7 },
+//   { title: "My Hero Academia", rating: 8.4 },
+//   { title: "Sword Art Online", rating: 7.6 },
+// ];
+
+// const animeList = animes.map(function (anime) {
+//   return anime.title.toUpperCase();
+// });
+// console.log(animeList);
