cd5f199d750f4c69d8cda1a2a2d40ecd32188227
unknown <bgsdewoo20@gmail.com>
2024-09-19 08:24:00 +0700

    Statement Return Pada Function

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 //Bab Function dan method Javascript
 
 //pengenalan function di JS
-function lemparDadu() {
-  console.log(Math.floor(Math.random() * 6) + 1);
-}
-lemparDadu();
+
+// function lemparDadu() {
+//   console.log(Math.floor(Math.random() * 6) + 1);
+// }
+// lemparDadu();
+
+//topic Statement Return Pada Function
+// function jumlahkan(a, b) {
+//   if (typeof a !== "number" || typeof b !== "number") {
+//     return "Bukan angka";
+//   }
+//   return a + b;
+// }
+
+// console.log(jumlahkan(1, 3));
