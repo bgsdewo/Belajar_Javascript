4df7e69a813ba044666d04d1a2d9625e4fa13815
unknown <bgsdewoo20@gmail.com>
2024-09-20 10:45:02 +0700

    Menggabungkan Nilai Array Dengan Array Lagi

--- script.js
+++ script.js
@@ -15,3 +15,9 @@
 // //lalu ingin mengambil nilai maks dari variabel angka menggunakan object Math.max tidak bisa
 // //harus melakukan ini dlu
 // console.log(Math.max(...angka));
+
+//topic Menggabungkan Nilai Array Dengan Array Lagi
+// const angka = [1, 2, 3, 4, 5];
+// const animals = ["Singa", "Sapi"];
+// const campuran = [...angka, ...animals, true];
+// console.log(campuran);
