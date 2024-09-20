09d50153fa61d98201e57b017523521673c9dac9
unknown <bgsdewoo20@gmail.com>
2024-09-20 10:31:38 +0700

    Mengatur Default Value Pada Function

---
+++ script.js
@@ -0,0 +1,9 @@
+//Bab beberapa fitur baru javascript
+
+//topic Mengatur Default Value Pada Function
+function sapa(namaDepan = "Bagus", namaBelakang = "Sadewo") {
+  console.log(
+    `Nama Depan Saya Adalah ${namaDepan}. Dan nama Belakang saya adalah ${namaBelakang}`
+  );
+}
+sapa();
